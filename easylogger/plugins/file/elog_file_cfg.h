75cd8806b821828d87f28ce7a5af843f8258573c
qintl <qintl@yytek.com>
2019-01-06 10:40:26 +0800

    add plugin file
    Signed-off-by: qintl <qintl@yytek.com>

---
+++ easylogger/plugins/file/elog_file_cfg.h
@@ -0,0 +1,13 @@
+#ifndef _ELOG_FILE_CFG_H_
+#define _ELOG_FILE_CFG_H_
+
+/* EasyLogger file log plugin's using file name */
+#define ELOG_FILE_NAME
+
+/* EasyLogger file log plugin's using file max size */
+#define ELOG_FILE_MAX_SIZE
+
+/* EasyLogger file log plugin's using semaphore key */
+#define ELOG_FILE_SEM_KEY
+
+#endif /* _ELOG_FILE_CFG_H_ */
---
+++ easylogger/plugins/file/elog_file_port.c
@@ -0,0 +1,52 @@
+#include "elog_file.h"
+
+/**
+ * EasyLogger flile log pulgin port initialize
+ *
+ * @return result
+ */
+ElogErrCode elog_file_port_init(void) {
+    ElogErrCode result = ELOG_NO_ERR;
+    
+    /* add your code here */
+
+    return result;
+}
+
+/**
+ * output file saved log port interface
+ *
+ * @param log file saved log
+ * @param size log size
+ */
+void elog_file_port_output(const char *log, size_t size) {
+    
+    /* add your code here */
+    
+}
+
+/**
+ * file log lock
+ */
+void elog_file_port_lock(void) {
+    
+    /* add your code here */
+    
+}
+
+/**
+ * file log unlock
+ */
+void elog_file_port_unlock(void) {
+    
+    /* add your code here */
+    
+}
+/**
+ * file log deinit
+ */
+void elog_file_port_deinit(void) {
+    
+    /* add your code here */
+    
+}
