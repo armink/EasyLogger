fbdc6c23b01daa0ebcd349bd9e0c16a76f2ad8ae
armink <armink.ztl@gmail.com>
2019-07-29 09:00:56 +0800

    【更新】软件版本号。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- easylogger/inc/elog.h
+++ easylogger/inc/elog.h
@@ -50,7 +50,7 @@ extern "C" {
 #define ELOG_LVL_TOTAL_NUM                   6
 
 /* EasyLogger software version number */
-#define ELOG_SW_VERSION                      "2.1.0"
+#define ELOG_SW_VERSION                      "2.1.99"
 
 /* EasyLogger assert for developer. */
 #ifdef ELOG_ASSERT_ENABLE
